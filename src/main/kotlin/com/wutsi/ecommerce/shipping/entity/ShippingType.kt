83cca3ab2d5ad144d75ab4e0820b7fb6901cfb39
wutsi <herve.tchepannou@wutsi.com>
2022-06-20 09:42:46 -0400

    update shipping-type

--- src/main/kotlin/com/wutsi/ecommerce/shipping/entity/ShippingType.kt
+++ src/main/kotlin/com/wutsi/ecommerce/shipping/entity/ShippingType.kt
@@ -2,8 +2,8 @@ package com.wutsi.ecommerce.shipping.entity
 
 import com.wutsi.ecommerce.order.entity.AddressType
 
-enum class ShippingType(val addressType: AddressType) {
-    IN_STORE_PICKUP(AddressType.NONE),
+enum class ShippingType(val addressType: AddressType?) {
+    IN_STORE_PICKUP(null),
     LOCAL_PICKUP(AddressType.POSTAL),
     LOCAL_DELIVERY(AddressType.POSTAL),
     INTERNATIONAL_SHIPPING(AddressType.POSTAL),
